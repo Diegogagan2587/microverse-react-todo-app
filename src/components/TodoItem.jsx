c255f41386a6d2cfbd281b92b301669a445313b1
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-10 16:21:46 -0600

    add TodoItem module

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,10 @@
+const TodoItem = ({ itemProp }) => {
+    return (
+        <li>
+            <input type="checkbox" />
+            {itemProp.title}
+        </li>
+    );
+};
+
+export default TodoItem;
