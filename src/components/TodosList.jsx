adc6acd02454ed8923418384266e017a3c055da7
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-10 16:59:02 -0600

    add module TodosList.jsx

---
+++ src/components/TodosList.jsx
@@ -0,0 +1,13 @@
+import TodoItem from "./TodoItem";
+
+const TodosList = ({todosProps, setTodos}) => {
+    return (
+        <ul>
+            {todosProps.map((todo) => (
+                <TodoItem key={todo.id} itemProp={todo} setTodos={setTodos} />
+            ))}
+        </ul>
+    );
+}
+
+export default TodosList;
