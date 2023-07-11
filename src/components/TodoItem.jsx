91ea12307b1e62874f2484ce0434a0b68c3a97fe
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-10 21:47:09 -0600

    add handleChange function within TodoItem component

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,26 @@
+// 6. TodoItem will render individual todos item
 const TodoItem = ({ itemProp, setTodos }) => {
+    const handleChange = (id) => {
+        console.log('cliked', id)//remove after debuggin
+        setTodos((prevState) => 
+            prevState.map((todo) => {
+                if (todo.id === id) {
+                    return {
+                        ...todo,
+                        completed: !todo.completed,
+                    };
+                }
+                return todo;
+            })
+        );
+    }
     return (
         <li>
-            <input type="checkbox" />
+            <input
+                type="checkbox"
+                checked={itemProp.completed}
+                onChangeCapture={() => handleChange(itemProp.id)}
+            />
             {itemProp.title}
         </li>
     );
