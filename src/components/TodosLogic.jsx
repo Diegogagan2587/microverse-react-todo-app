b995df523a38d65710745af61353026c89b03363
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-11 08:47:21 -0600

    install uuid, set unique Id for each task using uuid

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -2,21 +2,22 @@
 import { useState } from "react";
 import InputTodo from "./InputTodo";
 import TodosList from "./TodosList";
+import {v4 as uuidv4} from "uuid";
 
 const TodosLogic = () => {
     const [todos, setTodos] = useState([
         {
-            id: 1,
+            id: uuidv4(),
             title: 'Setup development environment',
             completed: true,
         },
         {
-            id: 2,
+            id: uuidv4(),
             title: 'Develop website and add content',
             completed: false,
         },
         {
-            id: 3,
+            id: uuidv4(),
             title: 'Deploy to live server',
             completed: false,
         },
@@ -35,7 +36,7 @@ const TodosLogic = () => {
         // update state with user's input
         console.log('should add item ===>>>',title)
         const newTodo = {
-            id: 4,
+            id: uuidv4(),
             title: title,
             completed: false,
         }
