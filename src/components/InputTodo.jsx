64153afcb1aa6eaf185b318f14394b5545a56130
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-11 07:16:45 -0600

    create state for user input, includes and onChange handleChange function

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+
 // 4. This will take the user's input
 const InputTodo = (itemProp) => {
+    const [title, setTitle] = useState('');
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    }
+
     return (
         <form>
             <input
                 type="text"
                 placeholder="Add Todo..."
+                onChange={handleChange}
             />
             <button>Submit</button>
         </form>
