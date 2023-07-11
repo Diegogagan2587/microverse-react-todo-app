83c1c3486839e700f7c253b0ba1ae16c611b8c8b
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-10 22:11:43 -0600

    add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+// 2. Header Display the todos heading text.
+
+const Header = () => {
+    return (
+        <>
+            <h1>To-do's</h1>
+            <p>Add all the task you want to complete</p>
+        </>
+    )
+}
+
+export default Header;
