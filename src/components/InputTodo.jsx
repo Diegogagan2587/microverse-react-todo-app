38c299717aeada31cc3c6a6138695cfcbc683b24
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-10 21:49:59 -0600

    add InputComponent into a module, type text

---
+++ src/components/InputTodo.jsx
@@ -0,0 +1,12 @@
+// 4. This will take the user's input
+const InputTodo = (itemProp) => {
+    return (
+        <>
+            <input
+                type="text"
+            />
+        </>
+    )
+}
+
+export default InputTodo;
