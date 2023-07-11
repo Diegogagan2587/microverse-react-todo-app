cbc46bb06f7da09d7131494dde15e8bbf2d2866e
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-10 22:52:47 -0600

    add submit todo button within inputTodo module

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,11 +1,13 @@
 // 4. This will take the user's input
 const InputTodo = (itemProp) => {
     return (
-        <>
+        <form>
             <input
                 type="text"
+                placeholder="Add Todo..."
             />
-        </>
+            <button>Submit</button>
+        </form>
     )
 }
 
